27e6a01078cb7823e6065666d3bac5ccba869821
alex-costich <alejandro.costich@edu.uag.mx>
2024-08-13 14:56:27 -0700

    Testing

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,11 @@
 import Kanban from '../components/kanban/Kanban';
+import UserList from '../components/vertical scroll list/UserList';
 
 const Home = () => {
 	return (
 		<div className='p-4'>
 			<Kanban />
+			<UserList />
 		</div>
 	);
 };
